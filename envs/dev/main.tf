81e5bb7f273ba665d6a088708dfa1020d08960ea
M <muraliprasad.murthy@schindler.com>
2026-02-28 01:36:48 +0530

    Add S3 security features for Checkov compliance

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -32,4 +32,36 @@ resource "aws_s3_bucket_versioning" "test" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  rule {
+    id     = "expire-old-versions"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
+}
+
 data "aws_caller_identity" "current" {}
